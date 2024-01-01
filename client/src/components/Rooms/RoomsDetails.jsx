d00344c8fb239cf7d7fba7aa3ef98dc33ed25275
unknown <freelancernayem.me@gmail.comm>
2024-01-01 18:59:07 +0600

    make css components

--- client/src/components/Rooms/RoomsDetails.jsx
+++ client/src/components/Rooms/RoomsDetails.jsx
@@ -2,29 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loader from '../Shared/Loader';
 import Container from '../Shared/Container';
+import { Helmet } from 'react-helmet-async';
+import Header from '../RoomDetails/Header';
 
 const RoomsDetails = () => {
-    const {id} = useParams()
+    const { id } = useParams()
     const [rooms, setRooms] = useState([])
-    const[loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-    
+
     useEffect(() => {
         setLoading(true)
-        fetch('https://raw.githubusercontent.com/Nayem-Ahmed/Hotelbooking-Client-Side/main/client/public/rooms.json')
+        fetch('/rooms.json')
             .then(res => res.json())
             .then(data => {
-                 const singledata = data.find(room => room._id === id)
-                 setRooms(singledata)
+                const singledata = data.find(room => room._id === id)
+                setRooms(singledata)
                 setLoading(false)
             })
 
 
     }, [id])
 
-    if (loading)  return <Loader></Loader>
+    if (loading) return <Loader></Loader>
     return (
-         <Container>{rooms?.title}</Container>
+
+        <Container>
+           <Helmet>
+            <title>{rooms.title}</title>
+            </Helmet>
+            <div>
+                <div className='flex flex-col gap-6'>
+                    <Header roomData={rooms}></Header>
+                </div>
+            </div>
+        </Container>
     );
 };
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 
+import { Helmet } from "react-helmet-async"
 import Categories from "../../components/Rooms/Categories"
 import Rooms from "../../components/Rooms/Rooms"
 
@@ -7,7 +8,7 @@ import Rooms from "../../components/Rooms/Rooms"
 const Home = () => {
   return (
     <div>
- 
+      <Helmet><title>StayVista | Vacation Homes & Condo Rentals</title></Helmet> 
      <Categories></Categories>
      <Rooms></Rooms>
 
